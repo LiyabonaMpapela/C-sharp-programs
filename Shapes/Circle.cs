f82491cef0b5b104957a5ffbb498e92496419e77
Liyabona Mpapela <167621411+LiyabonaMpapela@users.noreply.github.com>
2024-09-15 07:53:51 +0200

    Add files via upload
    I created two classes. One is a Shape..cs class with height and width instance variables and has a method to calculate its  area. The other class is a Circle.cs class which is a child class of the Shape class and inherits all its methods and attributes, it further has a PI attribute which stores the value of Math.PI. The Circle.cs class overrides the Area method to calculate the area of the circle .

---
+++ Shapes/Circle.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shapes
+{
+    internal class Circle :Shape
+    {
+        double  PI = Math.PI;
+
+        public Circle(double cordinate1) : base(cordinate1,0)
+         {
+          
+         }
+
+        public double Area()
+        {
+            return PI*height*height;
+        }
+    }
+}
---
+++ Shapes/Shape.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shapes
+{
+    internal class Shape
+    {
+
+        double _height = 0;
+        double _width = 0;
+
+        public double height
+        {
+            get { return _height; }
+            set { _height = height; }
+        }
+
+        public double width
+        {
+            get { return _width; }
+            set { _width = width; }
+        }
+
+        public Shape()
+        {
+         _height = 0;
+            _width = 0;
+        }
+
+        public Shape(double height, double width)
+        {
+            _height = height;
+            _width = width;
+        }
+
+        public double Area()
+            { return _height * _width; }
+
+
+    }
+}
